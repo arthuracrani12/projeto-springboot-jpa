57324571e20421eccf28c5eb62efece0606aad76
Arthur Acrani <arthuracrani12@gmail.com>
2024-01-26 20:39:31 -0300

    User entity and resource

---
+++ src/main/java/com/arthuracrani/course/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.arthuracrani.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arthuracrani.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	//controlador Rest que responde no caminhgo user
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Arthur", "arthur@gmail.com", "99999999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
+
+
