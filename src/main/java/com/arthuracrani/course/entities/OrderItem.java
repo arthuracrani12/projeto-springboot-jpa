054f3512d86cb22f3ab3d91cbbf8f4b4d4bcee36
Arthur Acrani <arthuracrani12@gmail.com>
2024-01-28 18:34:33 -0300

    criando OrderItem e associando com produto e pedido e criando classe auxiliar para identifcar item pedido

---
+++ src/main/java/com/arthuracrani/course/entities/OrderItem.java
@@ -0,0 +1,84 @@
+package com.arthuracrani.course.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+//mapeamentos JPA
+@Entity
+@Table(name = "tb_category")
+//seriazible para transformar o objeto em cadeias de bites para trafegar na rede
+public class OrderItem implements Serializable {
+
+	// número de série padrão
+	private static final long serialVersionUID = 1L;
+
+	// mostrar para o JPA qual a chave primaria do BD
+	@Id
+	// para a chave ser autoincrementada no BD
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+
+	//para evitar loop infinito de categoria buscando produto e produto buscando categoria
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+	public OrderItem() {
+
+	}
+
+	public OrderItem(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+}
